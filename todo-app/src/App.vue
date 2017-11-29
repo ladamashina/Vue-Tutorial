879a34c1934870dc77d850bfabf5d14250ab6fce
maksim.sarvas <ladamashina@gmail.com>
2017-11-29 18:16:45 +0300

    add files missed in previous commit

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,19 +1,49 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <TodoList v-bind:todos="todos"></TodoList>
+    <CreateTodo v-on:add-todo="addTodo"></CreateTodo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    TodoList,
+    CreateTodo,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        title,
+        done: false,
+      });
+    },
+  },
+};
 </script>
 
 <style>
